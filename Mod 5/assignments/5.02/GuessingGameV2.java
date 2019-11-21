a5f415705e623d0bdbdace4f74345b7d555e1da6
Josiah Kowalski <josiah.kowalski724@gmail.com>
2019-11-21 18:45:38 -0500

    l

--- Mod 5/assignments/5.02/GuessingGameV2.java
+++ Mod 5/assignments/5.02/GuessingGameV2.java
@@ -5,7 +5,7 @@
  * @version 11/20/19
  */
 import java.util.Scanner;
-public class GuessingGameV1
+public class GuessingGameV2
 {
     public static void main(String[] args) 
     {
@@ -14,13 +14,19 @@ public class GuessingGameV1
     double randomNum = Math.random();
     int randomNumFinal = 0;
     int guessNumber = 0;
+    int lowRange = 0;
+    int highRange = 0;
     int counter = 0;
     
     // acquiring proper range
-    randomNum += 0.01;
-    randomNumFinal = (int)(randomNum * 100);
-    System.out.println("\nGuess a number between 1 and 100\n");
+    System.out.println("Computer Guessing Game!");
+    System.out.println("\nEnter two natural numbers seperated by a space as your range (e.g. 20 50): ");
+        lowRange = in.nextInt();
+        highRange = in.nextInt();
+        highRange += 1;
+        randomNumFinal = (int) (((highRange - lowRange) * randomNum) + lowRange);
 
+    // while loop for guessing
     while(guessNumber != randomNumFinal)
     {
         System.out.print("Enter your guess: ");
