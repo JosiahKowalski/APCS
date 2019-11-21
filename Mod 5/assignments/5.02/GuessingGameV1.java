8dbb1a1225710d21fcf5d5b2d1fc6e512e6e339f
Josiah Kowalski <josiah.kowalski724@gmail.com>
2019-11-21 00:39:31 -0500

    "Mod 5/assignment:

---
+++ Mod 5/assignments/5.02/GuessingGameV1.java
@@ -0,0 +1,24 @@
+/**
+ * This program plays a guessing game with the user 
+ * 
+ * @author Josiah K.
+ * @version 11/20/19
+ */
+import java.util.Scanner;
+public class GuessingGameV1
+{
+    public static void main(String[] args) 
+    {
+    // define variables 
+    Scanner in = new Scanner(System.in);
+    
+
+
+
+
+
+
+        
+    }
+
+}
